a0cf63c700167c167108a937b05a98dad7d21cd7
Your Name <you@example.com>
2025-05-27 14:53:08 +0200

    Updated Test1.py - Bewegung aller Servos von min. zu max.

--- Test1.py
+++ Test1.py
@@ -7,15 +7,9 @@ pwm = Adafruit_PCA9685.PCA9685(busnum=1)
 servo_limits = {
     0: (100, 510),  # DM996 15kg
     1: (100, 510),  # DM996
-<<<<<<< Updated upstream
-    2: (205, 410),  # DM996
-    3: (150, 600),  # DS3225 25kg
-    4: (120, 500),  # 9g Microservo
-=======
     2: (100, 510),  # DM996
     3: (100, 510),  # DS3225 25kg
     4: (100, 510),  # 9g Microservo
->>>>>>> Stashed changes
 }
 
 # Frequency of the servos
@@ -24,19 +18,7 @@ pwm.set_pwm_freq(44)
 print('Moving servos (Testbetrieb)...')
 
 while True:
-<<<<<<< Updated upstream
-    # Move servo on channel O between extremes.
-    pwm.set_pwm(0, 0, servo_limits[0][0])
-    pwm.set_pwm(1, 0, servo_limits[0][0])
-    pwm.set_pwm(2, 0, servo_limits[1][0])
-    time.sleep(1)
-    pwm.set_pwm(0, 0, servo_limits[0][1])
-    pwm.set_pwm(1, 0, servo_limits[0][1])
-    pwm.set_pwm(2, 0, servo_limits[1][1])
-    time.sleep(1)
-=======
     for j in range(2):
         for i in range(5):
             pwm.set_pwm(i, 0, servo_limits[i][j])
         time.sleep(1)
->>>>>>> Stashed changes
