3cbd5ccc271ddf5ec6381776612da7bd3af997b4
CERATOPHRYS <leopromg@gmail.com>
2025-06-20 14:07:20 +0200

    Attempt to fix move_lin in Kinematics.py, reconfiguration of robot arm.

--- Kinematics.py
+++ Kinematics.py
@@ -7,9 +7,9 @@ class Kinematics:
     def __init__(self):
         self.Pose0 = Pose(0, 0, 0, 0, 0)
         self.Pose0.reset_Pose_to_auto_home()
-        self.joint_move_p2p(self.Pose0)
+        self.move_p2p(self.Pose0)
 
-    def lin_move_p2p(self, pos:Pose, speed:float):
+    def move_lin(self, target:Pose):
         """
         while self.Pose0.a0 != pos.a0 or self.Pose0.a1 != pos.a1 or self.Pose0.a2 != pos.a2 or self.Pose0.a3 != pos.a3 or self.Pose0.a4 != pos.a4:
             time1 = time.time()
@@ -21,30 +21,30 @@ class Kinematics:
             print(self.Pose0.posToString())
         print("finished!")
         """
+        speed=30
+        # Extrahiere Start- und Zielvektoren
+        p0 = np.array([self.Pose0.x, self.Pose0.y, self.Pose0.z, self.Pose0.r, self.Pose0.e])
+        p1 = np.array([target.x, target.y, target.z, target.r, target.e])
 
-        def lin_move_p2p(self, target:Pose, speed:float):
-            # Extrahiere Start- und Zielvektoren
-            p0 = np.array([self.Pose0.x, self.Pose0.y, self.Pose0.z, self.Pose0.r, self.Pose0.e])
-            p1 = np.array([target.x, target.y, target.z, target.r, target.e])
+        dist = np.linalg.norm(p1 - p0)  # Gesamtdistanz
+        duration = dist / speed if speed > 0 else 1  # Sekunden
+        steps = int(duration / 0.05)  # ca. 20 Hz, alle 50 ms
+        if steps < 1:
+            steps = 1
 
-            dist = np.linalg.norm(p1 - p0)  # Gesamtdistanz
-            duration = dist / speed if speed > 0 else 1  # Sekunden
-            steps = int(duration / 0.05)  # ca. 20 Hz, alle 50 ms
-            if steps < 1:
-                steps = 1
+        for i in range(steps + 1):
+            alpha = i / steps  # Fortschritt von 0 bis 1
+            interp = (1 - alpha) * p0 + alpha * p1
+            pose = Pose(*interp)
+            pose.get_axis_values()
+            self.Pose0 = pose
+            self.move_p2p(pose)
+            print(pose.posToString())
+            time.sleep(0.05)
 
-            for i in range(steps + 1):
-                alpha = i / steps  # Fortschritt von 0 bis 1
-                interp = (1 - alpha) * p0 + alpha * p1
-                pose = Pose(*interp)
-                self.Pose0 = pose
-                self.joint_move_p2p(pose)
-                print(pose.posToString())
-                time.sleep(0.05)
+        print("finished!")
 
-            print("finished!")
-
-    def joint_move_p2p(self, pos:Pose):
+    def move_p2p(self, pos:Pose):
         print(f"{pos.a0}, {pos.a1}, {pos.a2}, {pos.a3}, {pos.a4}")
         servo0 = Servo(0)
         servo1 = Servo(1)
@@ -56,4 +56,4 @@ class Kinematics:
         servo2.set_angle(pos.a2)
         servo3.set_angle(pos.a3)
         servo4.set_angle(pos.a4)
-        time.sleep(0.1)
+        time.sleep(0.05)
--- Test.py
+++ Test.py
@@ -1,16 +1,15 @@
 from Kinematics import Kinematics
 from Pose import Pose
 
-Pose1 = Pose(80, 0, 70, 5, True)
-Pose2 = Pose(40, 0, 70, 5, True)
+Pose1 = Pose(40, 20, 70, 5, True)
+Pose2 = Pose(40, -20, 70, 5, True)
 Kin = Kinematics()
 
-
 for i in range(3):
-    Kin.lin_move_p2p(Pose1, 30)
+    Kin.move_lin(Pose1)
 
-    Kin.lin_move_p2p(Pose2, 30)
+    Kin.move_lin(Pose2)
 
 Pose1.reset_Pose_to_auto_home()
-Kin.joint_move_p2p(Pose1)
+Kin.move_p2p(Pose1)
 print(Pose1.axisToString())
