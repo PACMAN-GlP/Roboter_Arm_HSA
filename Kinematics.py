96dd7fed1ca9008d69b8b75b0e115280ecac9b28
CERATOPHRYS <leopromg@gmail.com>
2025-06-30 17:04:46 +0200

    move_lin in Kinematics.py implemented and tested. Result: not usable due to mechanical imperfections, but mathematically in order

--- Kinematics.py
+++ Kinematics.py
@@ -9,27 +9,17 @@ class Kinematics:
         self.Pose0.reset_Pose_to_auto_home()
         self.move_p2p(self.Pose0)
 
-    def move_lin(self, target: Pose, steps: int = 50, wait_per_step: float = 0.01):
-        # Aktuelle Position aus self.Pose0 übernehmen
+    def move_lin(self, target:Pose, steps:int = 50, wait_per_step:float = 0.01):
         start = self.Pose0
         dx = (target.x - start.x) / steps
         dy = (target.y - start.y) / steps
         dz = (target.z - start.z) / steps
         dr = (target.r - start.r) / steps
-        de = (target.e - start.e) / steps  # Sollte e bool sein, wird es zu float 0.0 oder 1.0
-
+        de = (target.e - start.e) / steps
         for i in range(1, steps + 1):
-            # Zwischenschritt berechnen
-            intermediate_pose = Pose(
-                start.x + dx * i,
-                start.y + dy * i,
-                start.z + dz * i,
-                start.r + dr * i,
-                bool(round(start.e + de * i))  # wandelt zurück zu bool
-            )
-            self.move_p2p(intermediate_pose, wait=wait_per_step)
-
-        self.Pose0 = target  # Zielpose merken
+            zwischenPose = Pose(start.x + dx * i, start.y + dy * i, start.z + dz * i, start.r + dr * i, bool(round(start.e + de * i))) #convert to bool
+            self.move_p2p(zwischenPose, wait=wait_per_step)
+        self.Pose0 = target
 
     def move_p2p(self, pos:Pose, wait:float=1):
         print(f"{pos.a0}, {pos.a1}, {pos.a2}, {pos.a3}, {pos.a4}")
