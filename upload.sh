da8823ea14b56eea86f2c9b399aa681fe01170ab
CERATOPHRYS <leopromg@gmail.com>
2025-07-02 22:42:59 +0200

    unstable release: fully automated upload.sh. now requires installing sshpass! (linux: →sudo apt install sshpass)

--- upload.sh
+++ upload.sh
@@ -1,3 +1,2 @@
-scp Pose.py Test.py Servo.py Main.py Kinematics.py MainXYZ.py pi@pi.local:~
-ssh pi@pi.local
-
+sshpass -p "pi" scp Pose.py Test.py Servo.py Main.py Kinematics.py MainXYZ.py pi@pi.local:~
+sshpass -p "pi" ssh pi@pi.local
